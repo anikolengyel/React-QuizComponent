c466fcaac8f661f9fa8df9358d19eb9f8b0a5f7d
aniko.lengyel <lengyelaniko93@gmail.com>
2018-11-16 12:28:14 -0800

    created handler for answer button

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -4,11 +4,15 @@ class QuizQuestionButton extends Component {
   constructor(props) {
     super(props)
   }
+
+  handleClick() {
+    this.props.clickHandler(this.props.button_text)
+  }
   render() {
     return (
       <div>
       <li>
-      <button>{this.props.button_text}</button>
+      <button onClick= {this.handleClick.bind(this)}>{this.props.button_text}</button>
       </li>
       </div>
     )
