98bbb37d39ba5af1e203b4020817237623bf2079
aniko.lengyel <lengyelaniko93@gmail.com>
2018-11-15 20:19:46 -0800

    Completed the module

---
+++ src/QuizEnd.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+
+
+class QuizEnd extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+      <div>
+        <p>Thanks for playing!</p>
+        <a href=''>Reset Quiz</a>
+      </div>
+      </div>
+    )
+  }
+}
+
+export default QuizEnd
